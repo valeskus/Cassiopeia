ba8c4d734bc299804abd4f7b1010b08d1549ec27
valeskus <valeskus13@gmail.com>
2022-02-02 23:08:04 +0200

    Function Declaration

---
+++ vc_9_ac_2.js
@@ -0,0 +1,15 @@
+function getMessage(text = "Hello World") {
+    return (text)
+};
+getMessage();
+
+/*let text = "Hello World";
+function getHelloWorld() {
+   return(text)
+}*/
+
+
+/* function getMessage(text){
+     const message = "Hello World"; return(message);
+}
+getMessage(); */
