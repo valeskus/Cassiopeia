1511d96e4a6112e74e4255d3c963a6494071c32a
valeskus <valeskus13@gmail.com>
2022-02-02 20:35:35 +0200

    hz4

---
+++ vc_13_ac_4.js
@@ -0,0 +1,10 @@
+let color = prompt("Enter color name,red,blue or green", "");
+if (color == 'red') {
+    alert ( "red roses for ruby red rubies for rosie")
+} ;
+if (color == 'blue') {
+    alert ( "blue bluebird blinks")
+} ;
+if (color == 'green') {
+    alert ( "green grass globes glow greenly")
+} ;
