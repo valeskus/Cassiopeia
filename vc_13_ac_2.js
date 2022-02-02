88fe45a87148ed4ec600d6be62e2b73d855e9d93
valeskus <valeskus13@gmail.com>
2022-02-02 18:39:23 +0200

    hz2

---
+++ vc_13_ac_2.js
@@ -0,0 +1,14 @@
+let email = prompt("Enter your email", "");
+if (email.length < 3) {
+    alert("Not an email");
+}
+if (email.length > 3) {
+    alert("Message has been sent to the email");
+}
+
+/* let email = prompt("Enter your email", "");
+ if (email.length < 3) {
+     alert("Not an email"); }
+     else
+         alert( "Message has been sent to the email"); */
+
