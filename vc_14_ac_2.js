02b4a02bd1996d00d39023d29bbc9bdffb442322
valeskus <valeskus13@gmail.com>
2022-02-10 22:27:50 +0200

    Logical operators

---
+++ vc_14_ac_2.js
@@ -0,0 +1,21 @@
+let age;
+let height;
+let weight;
+function getAge(){
+    age = prompt("Enter your age") || undefined;
+}
+function getHeight(){
+    height = prompt("Enter your height") || undefined;
+}
+function getWeight(){
+    weight = prompt("Enter your weight") || undefined;
+}
+function showAll(){
+    getAge();
+    getHeight();
+    getWeight();
+
+    if (age !== undefined,  height !== undefined,  weight !== undefined) {
+        alert(`age: ${age}, height: ${height}, weight: ${weight}`);
+    }
+}
